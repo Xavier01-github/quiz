fc3e894a17158c96ef8d207ded0f411eb5e70694
Xavier01-github <schmutzxav@yahoo.com>
2024-06-19 08:42:36 +0200

    V1

---
+++ index.js
@@ -0,0 +1,60 @@
+function fonction1()    {
+    var afficherfaux =document.querySelector(".faux1");
+    afficherfaux.style.display = "block";
+}
+function fonction2()    {
+    var affichervrai=document.querySelector(".vrai1");
+    affichervrai.style.display = "block";
+    var vert=document.querySelector(".repvrai");
+    vert.style.color="green"
+}
+function fonction3()    {
+    var afficherfaux =document.querySelector(".faux2");
+    afficherfaux.style.display = "block";
+}
+function fonction4()    {
+    var affichervrai=document.querySelector(".vrai2");
+    affichervrai.style.display = "block";
+    var vert=document.querySelector(".repvrai2");
+    vert.style.color="green"
+}
+function fonction5()    {
+    var afficherfaux =document.querySelector(".faux3");
+    afficherfaux.style.display = "block";
+}
+function fonction6()    {
+    var affichervrai=document.querySelector(".vrai3");
+    affichervrai.style.display = "block";
+    var vert=document.querySelector(".repvrai3");
+    vert.style.color="green"
+}
+function fonction7()    {
+    var afficherfaux =document.querySelector(".faux4");
+    afficherfaux.style.display = "block";
+}
+function fonction8()    {
+    var affichervrai=document.querySelector(".vrai4");
+    affichervrai.style.display = "block";
+    var vert=document.querySelector(".repvrai4");
+    vert.style.color="green"
+}
+function fonction9()    {
+    var afficherfaux =document.querySelector(".faux5");
+    afficherfaux.style.display = "block";
+}
+function fonction10()    {
+    var affichervrai=document.querySelector(".vrai5");
+    affichervrai.style.display = "block";
+    var vert=document.querySelector(".repvrai5");
+    vert.style.color="green"
+}
+function fonction11()    {
+    var afficherfaux =document.querySelector(".faux6");
+    afficherfaux.style.display = "block";
+}
+function fonction12()    {
+    var affichervrai=document.querySelector(".vrai6");
+    affichervrai.style.display = "block";
+    var vert=document.querySelector(".repvrai6");
+    vert.style.color="green"
+}
